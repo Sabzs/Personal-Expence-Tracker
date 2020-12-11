221ee8559f051bb8bd58b142bdd57887ced8f674
Sabzs <sajiyoonline@gmail.com>
2020-12-11 12:28:39 -0800

    basic component layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { Balance } from './components/Balance';
 import { Header } from './components/Header';
+import { IncomeExpence } from './components/IncomeExpence';
+import { TransactionList } from './components/TransactionList';
 
 
 import './styles/App.css';
@@ -8,6 +11,13 @@ function App() {
   return (
     <div className="App">
       <Header />
+
+      <div>
+        <Balance />
+        <IncomeExpence />
+        <TransactionList />
+      </div>
+
     </div>
   );
 }
---
+++ client/src/components/IncomeExpence.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const IncomeExpence = () => {
+  return (
+    <div className="inc-exp-container" >
+      <div className="inc-exp-container__minusDiv" >
+        <h4 className="inc-exp-container__plus-header" >Income</h4>
+        <p className="inc-exp-container__money plus" >+0.00</p>
+      </div>
+      <div className="inc-exp-container__minusDiv" >
+        <h4 className="inc-exp-container__minus-header" >Expence</h4>
+        <p className="inc-exp-container__money minus" >+0.00</p>
+      </div>
+
+    </div>
+  )
+}
