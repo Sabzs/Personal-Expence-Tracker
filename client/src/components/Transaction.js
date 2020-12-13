42947a34c5b33367653c914cbdf91cdc508fd1fd
Sabzs <sajiyoonline@gmail.com>
2020-12-12 21:08:05 -0800

    logic to determine amount negative or positive

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -5,9 +5,10 @@ export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? "-" : "+";
 
   return (
-    <li className="transac-List__listItem">
+    <li className={transaction.amount < 0 ? "minus" : "plus"}
+    >
       {transaction.text}
-      <span className="transac-List__minus" >{sign}${Math.abs(transaction.amount)}  </span>
+      <span className="transac-List__span" >{sign}${Math.abs(transaction.amount)}  </span>
       <button className="transac-List__delete-btn">x</button>
     </li>
   )
