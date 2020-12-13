1ae729df57c4a8ae57effefd369f5b163ce448a5
Sabzs <sajiyoonline@gmail.com>
2020-12-13 11:29:17 -0800

    passed Add-new Action for adding new transactions

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -1,14 +1,32 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { GlobalContext } from '../context/GlobalContext';
+import { v4 as uuidv4 } from 'uuid';
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: uuidv4(),
+      text,
+      amount: +amount //the "+amount" will make sure total is not added as a string//
+    }
+    addTransaction(newTransaction)
+
+  }
+
 
   return (
-    <div className="addTransac" >
+    <div className="addTransac"  >
+
       <h3 className="addTransac__header" >Add new transaction</h3>
-      <form className="addTransac__form" >
+
+      <form className="addTransac__form" onSubmit={onSubmit} >
         <div className="addTransac__form-control">
           <label className="addTransac__label"
             htmlFor="text"  >Text</label>
