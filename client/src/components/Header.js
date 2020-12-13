cc2bf0e5d4f8e87ad6f472e1831443262d226dc7
Sabzs <sajiyoonline@gmail.com>
2020-12-13 14:06:27 -0800

    minor cleanup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
   return (
     <div className="headerDiv" >
-      <h1 className="HeaderDiv__title"  >Personal Expence Tracker</h1>
+      <h1 className="HeaderDiv__title"  >Personal Expense Tracker</h1>
     </div>
   )
 }
