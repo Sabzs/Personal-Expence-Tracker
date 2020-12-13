54be871a664e53298dd912e21ada0bd4682a6a36
Sabzs <sajiyoonline@gmail.com>
2020-12-12 21:34:27 -0800

    getting total income and expense

--- client/src/components/IncomeExpence.js
+++ client/src/components/IncomeExpence.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalContext'
+
 
 export const IncomeExpence = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map(transaction => transaction.amount)
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1
+  ).toFixed(2)
+
+
   return (
     <div className="inc-exp-container" >
       <div className="inc-exp-container__minus-plusDiv" >
         <h4 className="inc-exp-container__plus-header" >Income</h4>
-        <p className="inc-exp-container__money--plus" >+0.00</p>
+        <p className="inc-exp-container__money--plus" >+{income}</p>
       </div>
       <div className="inc-exp-container__minus-plusDiv" >
-        <h4 className="inc-exp-container__minus-header" >Expence</h4>
-        <p className="inc-exp-container__money--minus" >+0.00</p>
+        <h4 className="inc-exp-container__minus-header" >Expense</h4>
+        <p className="inc-exp-container__money--minus" >${expense}</p>
       </div>
 
     </div>
