83adfe2200cad0874da88cb8076324b7c95b016b
Sabzs <sajiyoonline@gmail.com>
2020-12-13 11:26:09 -0800

    added Add-new & Delete Action Functions to Provider

--- client/src/context/GlobalContext.js
+++ client/src/context/GlobalContext.js
@@ -6,10 +6,8 @@ import { v4 as uuidv4 } from 'uuid';
 //initial state
 const initialState = {
   transactions: [
-    { id: uuidv4(), text: "Flower", amount: -20 },
     { id: uuidv4(), text: "Salary", amount: 400 },
     { id: uuidv4(), text: "Book", amount: -20 },
-    { id: uuidv4(), text: "Camera", amount: 100 }
   ]
 }
 
@@ -20,9 +18,28 @@ export const GlobalContext = createContext(initialState)
 export const GlobalContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(GlobalReducer, initialState);
 
+  //actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction
+    });
+  }
+
 
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider value={{
+      transactions: state.transactions,
+      deleteTransaction,
+      addTransaction
+    }}>
       { children}
     </GlobalContext.Provider>
   );
