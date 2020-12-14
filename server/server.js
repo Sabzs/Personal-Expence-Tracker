3c4af424ccd30455e823a3c54362ec25b2dffd0d
Sabzs <sajiyoonline@gmail.com>
2020-12-13 17:41:59 -0800

    added routes and DB-connection instance to server

--- server/server.js
+++ server/server.js
@@ -3,22 +3,24 @@ const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
+const connectDB = require("./config/db");
+
 
 dotenv.config({ path: "./config/config.env" });
-
+connectDB();
 
 const app = express();
 app.use(cors());
 
 
-
-// const transactionsRoutes = require("./routes/transaction");
-// app.use("/api/v1/transactions", transactionsRoutes);
-
+const transactionsRoutes = require("./routes/transaction");
+app.use("/api/v1/transactions", transactionsRoutes);
 
 
 
 const PORT = process.env.PORT || 8080;
-app.listen(
-  PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.yellow.bold
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.yellow.bold
   ));
+
+
