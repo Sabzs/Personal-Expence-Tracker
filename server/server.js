0c254e51d004d7cca2a6c7d6c6e35588f790acaa
Sabzs <sajiyoonline@gmail.com>
2020-12-14 10:03:28 -0800

    added body-parser & minor cleanup

--- server/server.js
+++ server/server.js
@@ -2,18 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+// const bodyParser = require("body-parser");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
+const app = express();
+const transactionsRoutes = require("./routes/transaction");
 
 dotenv.config({ path: "./config/config.env" });
 connectDB();
 
-const app = express();
+
+app.use(express.json());
 app.use(cors());
-
-
-const transactionsRoutes = require("./routes/transaction");
 app.use("/api/v1/transactions", transactionsRoutes);
 
 
